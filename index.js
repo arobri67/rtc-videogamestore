9eb2510b284ec8b145bbdf7aca635c2f2f0749b8
Arnaud <arnaud.obri@proton.me>
2024-02-03 16:10:30 +0100

    implement express server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/api/v1/", (req, res, next) => {
+  return res.status(200).json({
+    data: "Works!",
+  });
+});
+
+//next should be present and is good practice. example for the token without the next you check for token but do not go next.
+app.use("*", (req, res, next) => {
+  return res.status(404).json({ data: "404 not found" });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
